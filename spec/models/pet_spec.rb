08dfbb209e166ac743cd3937b6211d0699d625b3
Sho <sfaizi1@outlook.com>
2016-11-24 09:44:50 -0800

    further rspec

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -5,31 +5,31 @@ RSpec.describe Pet, type: :model do
  describe 'validations' do
 
    it 'will have a pet type' do
-     p = Pet.new
+     p = build(:pet, pet_type: nil)
      p.valid?
      expect(p.errors).to have_key(:pet_type)
    end
 
    it 'will have a name' do
-     n = Pet.new
+     n = build(:pet, name: nil)
      n.valid?
      expect(n.errors).to have_key(:name)
    end
 
    it 'will have a color' do
-     c = Pet.new
+     c = build(:pet, color: nil)
      c.valid?
      expect(c.errors).to have_key(:color)
    end
 
    it 'will have a gender' do
-     p = Pet.new
+     p = build(:pet, gender: nil)
      p.valid?
      expect(p.errors).to have_key(:gender)
    end
 
    it 'will have a last_seen_at' do
-     p = Pet.new
+     p = build(:pet, last_seen_at: nil)
      p.valid?
      expect(p.errors).to have_key(:last_seen_at)
    end
