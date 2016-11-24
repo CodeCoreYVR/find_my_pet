4241ea691872d9b2fcebd093eddfa65e2c135aaf
Benjamin Brenner <ben.l.brenner@gmail.com>
2016-11-24 10:36:41 -0800

    Sighting model tested

--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    it 'has a pet type' do
+      s = build(:sighting, pet_type: nil)
+      s.valid?
+      expect(s.errors).to have_key(:pet_type)
+    end
+
+    it 'has a last_seen_at' do
+      s = build(:sighting, last_seen_at: nil)
+      s.valid?
+      expect(s.errors).to have_key(:last_seen_at)
+    end
+
+    it 'it has a date_time' do
+      s = build(:sighting, date_time: nil)
+      s.valid?
+      expect(s.errors).to have_key(:date_time)
+    end
+
+  end
+
+  describe 'associations' do
+
+    it 'belongs_to pet' do
+      x = create(:pet)
+      s = x.sightings.create
+      expect(s.pet_id).to eq(x.id)
+    end
+
+  end
+
 end
