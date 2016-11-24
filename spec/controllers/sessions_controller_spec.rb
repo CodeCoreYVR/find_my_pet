4cd83cef246e0e9ca24468b43b78f772ef7faf34
Reuben Jones <mrreubenjones@gmail.com>
2016-11-24 09:52:40 -0800

    Test sessions controller with RSPEC

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:user) {create(:user)}
+
   describe '#create' do
-
-    context 'With existing User' do
-      let(:user) {create(:user)}
-      it 'redirects to Home page if right credentials' do
-        post :create, params: {email: user.email, password: user.password}
-        expect(response).to redirect_to(home_path)
-      end
-
-      it 'render new session page if wrong credentials' do
-        post :create, params: {email: nil, password: user.password}
-        expect(response).to render_template(:new)
-      end
-
+    it 'redirects to Home page if right credentials' do
+      post :create, params: {email: user.email, password: user.password}
+      expect(response).to redirect_to(home_path)
     end
 
-    context 'Without existing User' do
-      it 'render new session page if wrong credentials'
+    it 'render new session page if wrong credentials' do
+      post :create, params: {email: nil, password: user.password}
+      expect(response).to render_template(:new)
     end
+  end
 
+  describe '#destroy' do
+    it 'redirects to Home page' do
+      request.session[:user_id] = user.id
+      delete :destroy, params: {user_id: user.id}
+      expect(response).to redirect_to(home_path)
+    end
   end
 
 end
