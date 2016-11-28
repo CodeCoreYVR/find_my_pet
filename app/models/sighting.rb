c01ff2c3df6be605b51c034f0ea3d38fdce54534
Steve Godin <steve.god@gmail.com>
2016-11-28 13:22:35 -0800

    Correct remaining rubocop offense

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,6 +1,6 @@
 class Sighting < ApplicationRecord
   extend FriendlyId
-  
+
   belongs_to :pet
   validates :pet_type, presence: true
   validates :last_seen_at, presence: true
@@ -9,7 +9,7 @@ class Sighting < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   friendly_id :slug_candidates, use: [:slugged, :history]
-  
+
   def slug_candidates
     [:pet_type, :last_seen_at]
   end
