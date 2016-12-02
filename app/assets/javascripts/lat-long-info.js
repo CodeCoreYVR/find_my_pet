c1f05f03de6f3584818c58eae0f8f076c577d801
octothorpus <octothorpus@gmail.com>
2016-12-02 12:00:06 -0800

    added jQuery

--- app/assets/javascripts/lat-long-info.js
+++ app/assets/javascripts/lat-long-info.js
@@ -10,11 +10,11 @@ $(function () {
 
       var fr = new FileReader()
       fr.addEventListener('load', function () {
-        var thumbnailContainer = document.getElementById('img-thumb')
-        var thumbnail = document.createElement('img')
-        thumbnail.src = fr.result
+        var thumbnailContainer = $('#img-thumb')
+        var thumbnail = $('<img></img>')
+        thumbnail.attr('src', fr.result)
 
-        thumbnailContainer.appendChild(thumbnail)
+        thumbnailContainer.html(thumbnail)
 
         getExif(thumbnail, function (longitude, latitude) {
           $('[name*=lat]').val(latitude)
@@ -28,23 +28,20 @@ $(function () {
 
 function getExif (img, cb) {
   // if cb is not a function, log an error
-  if (typeof (cb) === 'function') {
-    EXIF.getData(img, function () {
-      var longitude = EXIF.getTag(this, 'GPSLongitude')
-      var latitude = EXIF.getTag(this, 'GPSLatitude')
-      // var latAndLong = document.getElementById('latAndLong')
-      // latAndLong.innerHTML = `${latitude},  ${longitude}`
-      var decLat = degToDec(latitude)
-      var decLong = degToDec(longitude)
-      cb(decLat, decLong)
-    })
-  } else {
-    window.alert('there is no function')
+  if (typeof (cb) !== 'function') {
+    throw new TypeError('Expected second arguement to be a function, but got ' + typeof (cb))
   }
+
+  EXIF.getData(img, function () {
+    var longitude = EXIF.getTag(this, 'GPSLongitude')
+    var latitude = EXIF.getTag(this, 'GPSLatitude')
+    var decLat = degToDec(latitude)
+    var decLong = degToDec(longitude)
+    cb(decLat, decLong)
+  })
 }
 
 // Turns the 'degrees, minutes, seconds' value from the EXIF data into a float.
-
 function degToDec (dmsValue) {
   dmsValue[1] = (dmsValue[2] / 60) + dmsValue[1]
   var decDeg = (dmsValue[1] / 60) + dmsValue[0]
