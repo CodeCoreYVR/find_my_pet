74c3ef165d0109f55da4ed7a5c18e3e42c81005b
Ricky Zhu <riozhu@gmail.com>
2016-12-08 15:35:33 -0800

    Add specs for Message & cleanup deprecation warnings (#25)
    * add rspec and factorygirl for pet and user
    
    * test create method for message controller
    
    * rspec for test message controller
    
    * Stub out Geocoder requests
    
    * Fix deprecation warnings
    
    * Refactor & cleanup tests

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,12 @@ class MessagesController < ApplicationController
     message_params = params.require(:message).permit(:body)
     @message = Message.new message_params
     @message.pet = @pet
+
     respond_to do |format|
       if @message.save
         MessagesMailer.message_pet_owner(@message).deliver_now
         format.js { render :create_success }
-        format.html { redirect_to :back }
+        format.html { redirect_back(fallback_location: pet_path(@pet)) }
       else
         format.js { render :create_failure }
         format.html { redirect_to :back, alert: 'Could not create message' }
@@ -18,9 +19,16 @@ class MessagesController < ApplicationController
 
   def destroy
     @message = Message.find params[:id]
+    pet = @message.pet
+
     respond_to do |format|
       if @message.destroy
-        format.html { redirect_to :back, notice: 'Answer deleted!' }
+        format.html do
+          redirect_back(
+            fallback_location: pet_path(pet),
+            notice: 'Answer deleted!'
+          )
+        end
         format.js { render }
       else
         format.html { redirect_to home_path, alert: 'access denied!' }
--- spec/controllers/api/v1/sightings_controller_spec.rb
+++ spec/controllers/api/v1/sightings_controller_spec.rb
@@ -7,25 +7,27 @@ RSpec.describe Api::V1::SightingsController, type: :controller do
 
   describe '#index' do
     it 'responds with 200 code' do
-      get :index, format: :json, id: pet.id
+      get :index, format: :json, params: { id: pet.id }
       expect(response).to be_success
     end
 
     it 'returns the correct JSON' do
       FactoryGirl.create :sighting, pet_id: pet.id, lat: 49.0
-      get :index, format: :json, id: pet.id
+
+      get :index, format: :json, params: { id: pet.id }
       json_response = JSON.parse(response.body).first
       expect(json_response['lat']).to eq(49.0)
     end
 
     it 'returns an empty array when there are no sightings' do
-      get :index, format: :json, id: pet.id
+      get :index, format: :json, params: { id: pet.id }
       expect(JSON.parse(response.body)).to eq([])
     end
 
     it 'returns a json of correct length' do
       FactoryGirl.create_list(:sighting, 3, pet_id: pet.id)
-      get :index, format: :json, id: pet.id
+
+      get :index, format: :json, params: { id: pet.id }
       expect(JSON.parse(response.body).length).to eq(3)
     end
   end
